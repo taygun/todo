c864b2e2dfe4a23c441968b8aba105233a886def
taygun <taygunagiali@gmail.com>
2013-06-17 14:28:04 +0300

    Changed some settings

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -92,6 +92,11 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+	'django.contrib.messages.context_processors.messages',
+	'django.contrib.auth.context_processors.auth',
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
