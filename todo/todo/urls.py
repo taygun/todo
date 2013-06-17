8f0f111a4699e967ac4d3af9f48e94feeee25bc9
taygun <taygunagiali@gmail.com>
2013-06-17 14:45:09 +0300

    Added some urls

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -1,15 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from todo.models import Task
+from todo import views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'todo.views.home', name='home'),
     # url(r'^todo/', include('todo.foo.urls')),
-
+	url(r'^$', views.index),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^login/$', 'django.contrib.auth.views.login'),
 
     # Uncomment the next line to enable the admin:
-     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
