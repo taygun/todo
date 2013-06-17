91f5c4205c665738eaa7397749acf8b3969a14af
taygun <taygunagiali@gmail.com>
2013-06-17 17:00:22 +0300

    Stricat

--- todo/todo/models.py
+++ todo/todo/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django import forms
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Task(models.Model):
+	user = models.ForeignKey(User)
 	name = models.CharField(max_length=200)
 	due_date = models.DateTimeField('due to date')
 	done = models.BooleanField(default=False)
 
+	def __unicode__(self):
+		return self.name
+
 class TaskForm(forms.Form):
 	name = forms.CharField(max_length=200)
 	due_date = forms.DateTimeField(required=False)
+
--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,30 +1,11 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, redirect,render
 from django.contrib.auth import authenticate, login
-
+from todo.models import Task
 
 
 def index(request):
-	return render(request, 'mytodolist.html', {'todo' : 'nimic'})	
+	Tasks = Task.objects.all()	
+	return render(request, 'mytodolist.html', {'Tasks' : Tasks})	
 	
 
-def login(request):
-	username = request.POST['username']
-	password = request.POST['password']
-	user = authenticate(username=username, password=password)
-	if user is not None:
-		if user.is_active:
-			login(request, user)
-			state = "Logged in"
-			return redirect('http://google.ro')
-				
-		else:
-			state = "Not active"
-	else:
-		state = "Your username/password was incorrect"
-	return render_to_response('login.html',
-			{'state':state,
-			'username':username,
-			},
-			context_instance=RequestContext(request)
-			)	
