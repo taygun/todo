e5fc0667946382f4b0c410fa9a5e33a462b2af07
taygun <taygunagiali@gmail.com>
2013-06-17 14:09:42 +0300

    Deleted mytodolist

--- todo/mytodolist/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Task(models.Model):
-	name = models.CharField(max_length=200)
-	due_date = models.DateTimeField('due to date')
-	done = models.BooleanField(default=False)
-
